e9d813730fec59696a42536bfc105fe4b3b855b7
felipebuso <felipe.buso@hotmail.com>
2025-05-22 10:50:01 -0300

    feat: Model User

---
+++ src/edu/felipebuso/model/User.java
@@ -0,0 +1,33 @@
+package edu.felipebuso.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private String email;
+    private String city;
+
+    public User(String name, String email, String city) {
+        this.name = name;
+        this.email = email;
+        this.city = city;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuário: " + name + " | Email: " + email + " | Cidade: " + city;
+    }
+}
